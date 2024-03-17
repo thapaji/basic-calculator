875bb04cd871d21da79748ea0208a27a16a35552
Sujan <mail.thapasujan@gmail.com>
2024-03-17 16:44:29 +1100

    Keypress event added and implemented

--- script.js
+++ script.js
@@ -6,42 +6,57 @@ let prevOperator = false;
 let arrButtons = document.querySelectorAll('.btn');
 arrButtons.forEach(button => {
     button.addEventListener('click', () => {
-        eventVal = button.innerText;
-        switch (eventVal) {
-            case 'AC':
-                stringToDisplay = "";
-                break;
-            case 'C':
-                stringToDisplay = stringToDisplay.slice(0, -1);
-                break;
-            case '=':
-                evaluate();
-                break;
-            case '+':
-            case '-':
-            case '/':
-            case '%':
-            case '*':
-                if (operators.includes(eventVal)) {
-                    prevOperator === true ? stringToDisplay = stringToDisplay.slice(0, -1) : prevOperator = true;
-                }
-
-                evaluate();
-                stringToDisplay += eventVal;
-                break;
-            case '.':
-                if(stringToDisplay.includes('.')){
-                    return;
-                }
-            default:
-                prevOperator = false;
-                stringToDisplay += eventVal;
-        }
+        const eventVal = button.innerText;
+        switchAndValidate(eventVal);
         display();
 
     });
 });
 
+window.addEventListener('keypress',(e)=>{
+    const eventVal = e.key;
+    if(!e.code.includes('Key')){
+        switchAndValidate(eventVal);
+        display();
+    }
+});
+
+const switchAndValidate = (eventVal) => {
+    switch (eventVal) {
+        case 'AC':
+            stringToDisplay = "";
+            break;
+        case 'C':
+            stringToDisplay = stringToDisplay.slice(0, -1);
+            break;
+        case 'Enter':
+        case '=':
+            if( prevOperator === true){
+                stringToDisplay = stringToDisplay.slice(0,-1);
+            }
+            evaluate();
+            break;
+        case '+':
+        case '-':
+        case '/':
+        case '%':
+        case '*':
+            if (operators.includes(eventVal)) {
+                prevOperator === true ? stringToDisplay = stringToDisplay.slice(0, -1) : prevOperator = true;
+            }
+
+            evaluate();
+            stringToDisplay += eventVal;
+            break;
+        case '.':
+            if (stringToDisplay.includes('.')) {
+                return;
+            }
+        default:
+            prevOperator = false;
+            stringToDisplay += eventVal;
+    }
+}
 
 const display = () => {
     stringToDisplay === '' ? result.innerText = '0.0' : result.innerText = stringToDisplay;
@@ -51,5 +66,5 @@ const evaluate = () => {
     if (stringToDisplay === '') {
         stringToDisplay = 0.0;
     }
-    stringToDisplay = eval(stringToDisplay);
+    stringToDisplay = eval(stringToDisplay) + '';
 }
